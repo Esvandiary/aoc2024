17b94a5ea010212cda4a01f724c279b4eafc3d9a
Andy Martin <andy@infinium.org.uk>
2024-12-10 01:28:21 +0000

    Day7: uno reverse card

--- Day7/main.c
+++ Day7/main.c
@@ -9,10 +9,8 @@
 #define PART1 (1 << 0)
 #define PART2 (1 << 1)
 
-static const int64_t nummuls[] = {1, 10, 100, 1000, 10000, 100000, 1000000};
-
 static int64_t numbers[64];
-static int numlens[64];
+static int nummuls[64];
 
 static bool is_possible_p1(int count, int64_t target, int n, int64_t running_total)
 {
@@ -33,7 +31,33 @@ static bool is_possible_p2(int count, int64_t target, int n, int64_t running_tot
         return false;
     return is_possible_p2(count, target, n + 1, running_total + numbers[n])
         || is_possible_p2(count, target, n + 1, running_total * numbers[n])
-        || is_possible_p2(count, target, n + 1, running_total * nummuls[numlens[n]] + numbers[n]);
+        || is_possible_p2(count, target, n + 1, running_total * nummuls[n] + numbers[n]);
+}
+
+static bool is_possible_reverse_p1(int count, int64_t running_total, int n)
+{
+    if (n == 0)
+        return (running_total == numbers[0]);
+
+    if (running_total >= numbers[n] && is_possible_reverse_p1(count, running_total - numbers[n], n-1))
+        return true;
+    if ((running_total % numbers[n]) == 0 && is_possible_reverse_p1(count, running_total / numbers[n], n-1))
+        return true;
+    return false;
+}
+
+static bool is_possible_reverse_p2(int count, int64_t running_total, int n)
+{
+    if (n == 0)
+        return (running_total == numbers[0]);
+
+    if (running_total >= numbers[n] && is_possible_reverse_p2(count, running_total - numbers[n], n-1))
+        return true;
+    if ((running_total % numbers[n]) == 0 && is_possible_reverse_p2(count, running_total / numbers[n], n-1))
+        return true;
+    if ((running_total % nummuls[n]) == numbers[n] && is_possible_reverse_p2(count, running_total / nummuls[n], n-1))
+        return true;
+    return false;
 }
 
 int main(int argc, char** argv)
@@ -57,23 +81,23 @@ int main(int argc, char** argv)
         {
             ++idx; // ' '
             numbers[count] = 0;
-            numlens[count] = 0;
+            nummuls[count] = 1;
             while (idx < fileSize && isdigit(file.data[idx]))
             {
                 numbers[count] = (numbers[count] * 10) + (file.data[idx++] & 0xF);
-                ++numlens[count];
+                nummuls[count] *= 10;
             }
             ++count;
         }
         while (idx < fileSize && !isdigit(file.data[idx]))
             ++idx;
 
-        if (is_possible_p1(count, num0, 1, numbers[0]))
+        if (is_possible_reverse_p1(count, num0, count-1))
         {
             sum1 += num0;
             sum2 += num0;
         }
-        else if (is_possible_p2(count, num0, 1, numbers[0]))
+        else if (is_possible_reverse_p2(count, num0, count-1))
         {
             sum2 += num0;
         }
