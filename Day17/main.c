1754f858cf68d5f38af1b3249c2d3a28e67466a2
Andy Martin <andy@infinium.org.uk>
2024-12-17 20:25:57 +0000

    D17P2, pain

--- Day17/main.c
+++ Day17/main.c
@@ -1,4 +1,3 @@
-// #define ENABLE_DEBUGLOG
 #include "../common/mmap.h"
 #include "../common/print.h"
 #include "../common/view.h"
@@ -6,6 +5,10 @@
 #include <stdbool.h>
 
 #define isdigit(c) ((c) >= '0' && (c) <= '9')
+
+#if !defined(min)
+#define min(x,y) (((x) < (y)) ? (x) : (y))
+#endif
 
 #define RA 0
 #define RB 1
@@ -23,7 +26,7 @@ typedef enum opcode
     OP_CDV = 7, // RA / 2**COP -> C
 } opcode;
 
-static const char* opnames[] = {
+static const char* const opnames[] = {
     "adv",
     "bxl",
     "bst",
@@ -35,15 +38,123 @@ static const char* opnames[] = {
 };
 
 static int64_t reg0[3];
-static int64_t reg[3];
 
-static inline FORCEINLINE int64_t comboop(uint8_t operand)
+static inline void printint(uint64_t n)
 {
-    return (operand < 4) ? operand : reg[operand-4];
+    int bits = n >> 58;
+    n &= ((uint64_t)1 << 58) - 1;
+    if (bits <= 0)
+        return;
+
+    bits -= 3;
+    putchar(0x30 + ((n >> bits) & 7));
+    bits -= 3;
+    for (; bits >= 0; bits -= 3)
+    {
+        putchar(',');
+        putchar(0x30 + ((n >> bits) & 7));
+    }
+
+    putchar('\n');
 }
 
-static char outbuf1[8192];
-static char outbuf1count;
+#define COMBOOP(operand) (((operand) < 4) ? (operand) : reg[(operand)-4])
+
+#define ADDNUM(x, y) (((uint64_t)(x) << 3) | (uint64_t)(y))
+#define SETBITS(x, n) ((uint64_t)(x) | ((uint64_t)(n) << 58))
+#define GETNUM(x, n) (((uint64_t)(x) >> (((uint64_t)(x) >> 58) - (uint64_t)(n)*3 - 3)) & 0x7)
+
+static uint64_t run(const char* data, const int dlen, int64_t reg[3])
+{
+#if defined(RUNSPAM)
+    int64_t a = reg[RA], b = reg[RB], c = reg[RC];
+#endif
+
+    uint64_t outint = 0;
+    uint8_t outintbits = 0;
+
+    int idx = 0;
+    while (idx + 3 <= dlen)
+    {
+        uint8_t opcode = data[idx] & 0xF;
+        idx += 2;
+        uint8_t operand = data[idx] & 0xF;
+        idx += 2;
+
+#if defined(RUNSPAM)
+        if (RUNSPAM)
+        {
+            DEBUGLOG("[A:% 10" PRId64 "][B:% 10" PRId64 "][C:% 10" PRId64 "] processing: %s %u (%" PRId64 ")\n",
+                reg[RA], reg[RB], reg[RC], opnames[opcode], operand, COMBOOP(operand));
+        }
+#endif
+
+        switch (opcode)
+        {
+            case OP_ADV: // A / 2**COP -> A
+                reg[RA] = reg[RA] / ((int64_t)1 << COMBOOP(operand));
+                break;
+            case OP_BXL: // B ^ OP -> B
+                reg[RB] = reg[RB] ^ (int64_t)operand;
+                break;
+            case OP_BST: // COP % 8 -> B
+                reg[RB] = COMBOOP(operand) & 7;
+                break;
+            case OP_JNZ: // (A != 0) ? jmp(OP) : nop
+                if (reg[RA])
+                    idx = operand*2;
+                break;
+            case OP_BXC: // B ^ C -> B
+                reg[RB] = reg[RB] ^ reg[RC];
+                break;
+            case OP_OUT: // print(COP % 8)
+                outint = ADDNUM(outint, COMBOOP(operand) & 7);
+                outintbits += 3;
+                break;
+            case OP_BDV: // A / 2**COP -> B
+                reg[RB] = reg[RA] / ((int64_t)1 << COMBOOP(operand));
+                break;
+            case OP_CDV: // A / 2**COP -> C
+                reg[RC] = reg[RA] / ((int64_t)1 << COMBOOP(operand));
+                break;
+        }
+    }
+
+    return SETBITS(outint, outintbits);
+}
+
+static uint8_t p2ref[16] = {0};
+static int p2refcount = 0;
+
+static uint64_t p2refint;
+static uint64_t p2refint2;
+
+static uint64_t findp2(uint64_t p2, uint8_t n, const char* data, int dlen)
+{
+    int64_t reg[3];
+    uint8_t ta = 0;
+    uint64_t result = UINT64_MAX;
+    for (ta = 0; ta < 8; ++ta)
+    {
+        uint64_t cpa = ADDNUM(p2, ta);
+        reg[RA] = cpa; reg[RB] = reg0[RB]; reg[RC] = reg0[RC];
+        DEBUGLOG("[P2] n %u, testing a = %" PRIu64 "\n", n, reg[RA]);
+        uint64_t testp2 = run(data, dlen, reg);
+        uint8_t testn = GETNUM(testp2, 0);
+        uint8_t refn = GETNUM(p2refint, p2refcount - n - 1);
+        DEBUGLOG("    testing %" PRIu64 " vs %u\n", (testp2 & 7), refn);
+        if (testn == refn)
+        {
+            uint64_t curresult;
+            if (n == p2refcount - 1)
+                curresult = (testp2 == p2refint) ? cpa : UINT64_MAX;
+            else
+                curresult = findp2(cpa, n + 1, data, dlen);
+            result = min(result, curresult);
+        }
+    }
+    return result;
+}
 
 int main(int argc, char** argv)
 {
@@ -51,6 +162,7 @@ int main(int argc, char** argv)
     const int fileSize = (int)(file.size);
 
     int idx = 0;
+    const int endidx = fileSize - ((file.data[fileSize-1] == '\n') ? 1 : 0);
 
     idx += 12; // 'Register A: '
     while (isdigit(file.data[idx]))
@@ -65,52 +177,62 @@ int main(int argc, char** argv)
     
     DEBUGLOG("start: A %" PRId64 ", B %" PRId64 ", C %" PRId64 "\n", reg0[RA], reg0[RB], reg0[RC]);
 
-    memcpy(reg, reg0, sizeof(reg));
+    // P1
+    uint64_t p1 = run(file.data + idx, endidx - idx, reg0);
+    printint(p1);
 
-    const int idx0 = idx;
-    const int lastidx = fileSize - ((file.data[fileSize-1] == '\n') ? 1 : 0);
-    while (idx + 3 <= lastidx)
+#if defined(ENABLE_DEBUGLOG) && defined(PROGSPAM)
     {
-        uint8_t opcode = file.data[idx] & 0xF;
-        idx += 2;
-        uint8_t operand = file.data[idx] & 0xF;
-        idx += 2;
+        static const char* combostr[] = {"0", "1", "2", "3", "A", "B", "C"};
 
-        DEBUGLOG("[A:% 10" PRId64 "][B:% 10" PRId64 "][C:% 10" PRId64 "] processing: %s %u (%" PRId64 ")\n",
-            reg[RA], reg[RB], reg[RC], opnames[opcode], operand, comboop(operand));
-
-        switch (opcode)
+        int cidx = idx;
+        while (cidx + 2 < endidx)
         {
-            case OP_ADV: // A / 2**COP -> A
-                reg[RA] = reg[RA] / ((int64_t)1 << comboop(operand));
-                break;
-            case OP_BXL: // B ^ OP -> B
-                reg[RB] = reg[RB] ^ (int64_t)operand;
-                break;
-            case OP_BST: // COP % 8 -> B
-                reg[RB] = comboop(operand) & 7;
-                break;
-            case OP_JNZ: // (A != 0) ? jmp(OP) : nop
-                if (reg[RA])
-                    idx = idx0 + operand*2;
-                break;
-            case OP_BXC: // B ^ C -> B
-                reg[RB] = reg[RB] ^ reg[RC];
-                break;
-            case OP_OUT: // print(COP % 8)
-                outbuf1[outbuf1count++] = ',';
-                outbuf1[outbuf1count++] = 0x30 + (comboop(operand) & 7);
-                break;
-            case OP_BDV: // A / 2**COP -> B
-                reg[RB] = reg[RA] / ((int64_t)1 << comboop(operand));
-                break;
-            case OP_CDV: // A / 2**COP -> C
-                reg[RC] = reg[RA] / ((int64_t)1 << comboop(operand));
-                break;
+            uint8_t opcode = file.data[cidx] & 0xF;
+            cidx += 2;
+            uint8_t operand = file.data[cidx] & 0xF;
+            cidx += 2;
+
+            DEBUGLOG("%s %u | ", opnames[opcode], operand);
+            switch (opcode)
+            {
+                case OP_ADV: DEBUGLOG("A = A / 2**%s\n", combostr[operand]); break;
+                case OP_BXL: DEBUGLOG("B = B ^ %u\n", operand); break;
+                case OP_BST: DEBUGLOG("B = %s %% 8\n", combostr[operand]); break;
+                case OP_JNZ: DEBUGLOG("jnz(%u)\n", operand); break;
+                case OP_BXC: DEBUGLOG("B = B ^ C\n"); break;
+                case OP_OUT: DEBUGLOG("out(%s %% 8)\n", combostr[operand]); break;
+                case OP_BDV: DEBUGLOG("B = A / 2**%s\n", combostr[operand]); break;
+                case OP_CDV: DEBUGLOG("C = A / 2**%s\n", combostr[operand]); break;
+            }
         }
     }
+#endif
 
-    printf("%.*s\n", outbuf1count - 1, outbuf1 + 1);
+    // P2
+    for (int cidx = idx; cidx < endidx; cidx += 2)
+    {
+        // add in reverse
+        // p2refint |= (uint64_t)(file.data[cidx] & 7) << (p2refcount * 3);
+        p2refint = ADDNUM(p2refint, file.data[cidx] & 7);
+        p2ref[p2refcount++] = (uint64_t)(file.data[cidx] & 7);
+    }
+    p2refint = SETBITS(p2refint, p2refcount * 3);
+
+#if defined(DEBUGLOG)
+    DEBUGLOG("p2ref =");
+    for (int i = 0; i < p2refcount; ++i)
+        DEBUGLOG(" %u", p2ref[i]);
+    DEBUGLOG("\n");
+#endif
+
+    uint64_t p2 = findp2(0, 0, file.data + idx, endidx - idx);
+
+    int64_t reg[3] = {p2, reg0[1], reg0[2]};
+    uint64_t v = run(file.data + idx, endidx - idx, reg);
+    if ((v >> 58) % 2)
+        p2 *= 8;
+    print_uint64(p2);
 
     return 0;
 }
