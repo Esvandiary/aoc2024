3ae860da6093094f5ec1187f4811cf572daba39b
Andy Martin <andy@infinium.org.uk>
2024-12-18 15:29:53 +0000

    D18P2 better

--- Day18/main.c
+++ Day18/main.c
@@ -1,41 +1,53 @@
-// #define ENABLE_DEBUGLOG
 #include "../common/mmap.h"
 #include "../common/print.h"
 #include "../common/view.h"
 
+#include <stdbool.h>
+
 #define isdigit(c) ((c) >= '0' && (c) <= '9')
+
+typedef struct pos
+{
+    uint32_t x;
+    uint32_t y;
+} pos;
 
 static uint32_t grid[71][71];
 static uint32_t gridbest[71][71];
+static pos bestpath[1024];
+static uint32_t bestpathcount;
 static const uint32_t gw = 71;
 static const uint32_t gh = 71;
-static const int blockcount = 1024;
+static const int p1blockcount = 1024;
 
-typedef enum dir
+static bool p1steps(uint32_t x, uint32_t y, uint32_t ex, uint32_t ey, uint32_t steps)
 {
-    UP,
-    RIGHT,
-    DOWN,
-    LEFT
-} dir;
-
-static void p1steps(uint32_t x, uint32_t y, uint32_t ex, uint32_t ey, uint32_t steps)
-{
+    // DEBUGLOG("[%u] running for %u,%u\n", steps, x, y);
     if (!gridbest[y][x] || steps < gridbest[y][x])
         gridbest[y][x] = steps;
     else
-        return;
-    if (x == ex && y == ey)
-        return;
+        return false;
 
+    if (x == ex && y == ey)
+    {
+        bestpath[steps-1] = (pos) { x, y };
+        bestpathcount = steps;
+        return true;
+    }
+
+    bool result = false;
     if (x > 0 && !grid[y][x-1])
-        p1steps(x-1, y, ex, ey, steps + 1);
+        result = p1steps(x-1, y, ex, ey, steps + 1) || result;
     if (y > 0 && !grid[y-1][x])
-        p1steps(x, y-1, ex, ey, steps + 1);
+        result = p1steps(x, y-1, ex, ey, steps + 1) || result;
     if (x+1 < gw && !grid[y][x+1])
-        p1steps(x+1, y, ex, ey, steps + 1);
+        result = p1steps(x+1, y, ex, ey, steps + 1) || result;
     if (y+1 < gh && !grid[y+1][x])
-        p1steps(x, y+1, ex, ey, steps + 1);
+        result = p1steps(x, y+1, ex, ey, steps + 1) || result;
+
+    if (result)
+        bestpath[steps-1] = (pos) { x, y };
+    return result;
 }
 
 int main(int argc, char** argv)
@@ -63,32 +75,48 @@ int main(int argc, char** argv)
 
         ++grid[y][x];
 
-        if (++blocks == blockcount)
+        if (++blocks == p1blockcount)
         {
+            DEBUGLOG("[P1] running\n");
             p1steps(sx, sy, ex, ey, 1);
+            DEBUGLOG("[P1] done\n");
             uint64_t sum1 = gridbest[ey][ex] - 1;
             print_uint64(sum1);
-        }
-        else if (blocks > blockcount)
-        {
-            memset(gridbest, 0, sizeof(gridbest));
-            p1steps(sx, sy, ex, ey, 1);
-            if (!gridbest[ey][ex])
+
+#if defined(ENABLE_DEBUGLOG)
+            for (int y = 0; y < gh; ++y)
             {
-                printf("%u,%u\n", x, y);
-                break;
+                for (int x = 0; x < gw; ++x)
+                    DEBUGLOG("%c", grid[y][x] ? '#' : '.');
+                DEBUGLOG("\n");
+            }
+#endif
+        }
+        else if (blocks > p1blockcount)
+        {
+            bool breaksbest = false;
+            for (int i = 0; i < bestpathcount; ++i)
+            {
+                if (x == bestpath[i].x && y == bestpath[i].y)
+                {
+                    breaksbest = true;
+                    break;
+                }
+            }
+
+            if (breaksbest)
+            {
+                DEBUGLOG("[P2] block @ %u,%u breaks best, reticulating splines\n", x, y);
+                memset(gridbest, 0, sizeof(gridbest));
+                if (!p1steps(sx, sy, ex, ey, 1))
+                {
+                    DEBUGLOG("[P2] done\n");
+                    printf("%u,%u\n", x, y);
+                    break;
+                }
             }
         }
     }
-
-#if defined(ENABLE_DEBUGLOG)
-    for (int y = 0; y < gh; ++y)
-    {
-        for (int x = 0; x < gw; ++x)
-            DEBUGLOG("%c", grid[y][x] ? '#' : '.');
-        DEBUGLOG("\n");
-    }
-#endif
 
     return 0;
 }
